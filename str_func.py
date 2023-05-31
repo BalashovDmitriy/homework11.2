5fc9917c3b69f806169ba75aed2ec2fd1123b554
Dmitriy.Balashov <reaver_std@mail.ru>
2023-05-31 15:10:00 +0500

    changes in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,6 +1,19 @@
 def text_to_upper(text):
     """
-    do something
+    :param text: текст
+    :return: текст большими буквами
     """
     return text.upper()
+
+
+def first_characters_text_to_upper(text):
+    """
+    :param text: текст
+    :return: текст с заглавными буквами в каждом слове
+    """
+    words = text.split()
+    result = []
+    for word in words:
+        result.append(word.capitalize())
+    return " ".join(result)
 
